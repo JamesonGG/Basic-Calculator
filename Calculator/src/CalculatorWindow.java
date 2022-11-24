df77e3d78279f899715f362c84bc20e4a1ac6e3f
JamesonGG <95173860+JamesonGG@users.noreply.github.com>
2022-11-24 15:40:36 +1100

    Calculator runs in window
    ##2 new operations: imp2dec, and dec2imp. These conversions occur using a standard of 2.54cm : 1 inch.
    ##Operations may now be selected via dropdown menu.

--- Calculator/src/CalculatorWindow.java
+++ Calculator/src/CalculatorWindow.java
@@ -1,40 +1,83 @@
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-//import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
-public class CalculatorWindow extends JFrame {
-	public String output;
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CalculatorWindow frame = new CalculatorWindow();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+@SuppressWarnings("serial")
+public class CalculatorWindow extends JFrame{
+	public static int height;
+	public static int width;
+	public static JFrame frame;
+    public static JTextPane jta;
+    public static JTextField textA;
+    public static JTextField textB;
+    public static JTextField output;
+    public static JComboBox<?> operator;
+    public static JButton runButton;
+	
+	public static void main(String[] args) throws IOException {
+		Initialize();
+        
+        runButton.addActionListener(new ActionListener(){  
+        	public void actionPerformed(ActionEvent e){  
+        		String textValA = textA.getText();
+        		String textValB = textB.getText();
+                
+                Calculator.a = Float.parseFloat(textValA);
+                Calculator.b = Float.parseFloat(textValB);
+                Calculator.operation = (String) operator.getSelectedItem();
+                
+        		float result = Calculator.result(Calculator.operation, Calculator.a, Calculator.b);
+        	    String outputText = String.valueOf(result);
+        	    output.setText(outputText);
+        	}  
+        }); 
+        
+        
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public CalculatorWindow() {
+	
+	public static void Initialize() {
+		String[] op = new String[]{"add", "subtract", "multiply", "divide", "modulo", "power", "factorial", "sine", "cosine", "tangent", "absolute", "dec2imp", "imp2dec"};
+		height = 300;
+		width = 400;
 		
-	}
+		frame = new JFrame("Basic Calculator");
+		jta = new JTextPane();
+		textA = new JTextField("");
+		textB = new JTextField("");
+		output = new JTextField("");
+		runButton = new JButton("Compute");
+		operator = new JComboBox<Object>(op);
+		
+        textA.setBounds(width/4, height/14, width/4, height/9);
+        textB.setBounds(width/4, height/6, width/4, height/9);
+        operator.setBounds(width/4, 3*height/11, width/4, height/9);
+        
+        runButton.setBounds(width/4, 2*height/5, width/2, height/9);
+        output.setBounds(3*width/8, 2*height/3, width/4, height/9);
+        
+        //frame layout
+        frame.setLayout(new BorderLayout());
+        frame.add(jta,BorderLayout.CENTER);
+        frame.setSize(width, height);
+        
+        frame.add(textA);
+        frame.add(textB);
+        frame.add(runButton);
+        frame.add(output);
+        frame.add(operator);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    	frame.setResizable(false);
+    	output.setEditable(false);
+    }
 
 }
